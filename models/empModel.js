3ed2f05e659695ea0e64d632087f3a30c413a338
amanpawar6 <aman.pawar.7.1997@gmail.com>
2020-08-13 17:51:51 +0530

    commit to merge file aman

---
+++ models/empModel.js
@@ -0,0 +1,27 @@
+const mongoose=require("mongoose");
+
+const empSchema = new mongoose.Schema({
+    "email": String,
+    "password": String,
+    "name": String,
+    "age": Number,
+    "sc": String,
+    "ssc": String,
+    "graduation": String,
+    "masters": String,
+    "phone_no": Number,
+    "address": String,
+    "lst_cmpny": String,
+    "crt_cmpny": String,
+    "Designation": String,
+    "image" : String,
+    "post" : [{ 
+        "post" : {type:String},
+        "time" : { type : Date, default: Date.now }
+    }]
+});
+
+const empModel = mongoose.model('emp', empSchema);
+
+module.exports = empModel;
+
